46dc0c847a87954477057ff258545adb0a726bee
EstherMutua <esthermutua16@gmail.com>
2025-06-29 13:10:23 +0300

    refactor code to use flask_restx for api endpoints

--- server/app.py
+++ server/app.py
@@ -2,49 +2,56 @@ from flask import Flask, request, make_response, jsonify
 from models import db, Hero, Power, HeroPower
 from flask_migrate import Migrate
 from flask_cors import CORS
+from flask_restx import Api, Resource
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///superheroes.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.json.compact = False
 
+api = Api(app)
 CORS(app)
 migrate = Migrate(app, db)
 
 db.init_app(app)
 
-@app.route('/')
-def index():
-    return '<h1>Superheroes API</h1>'
+@api.route('/')
+class index(Resource):
+    def get(self):
+        return make_response({"message": "Welcome to the Superheroes API"}, 200, {'Content-Type': 'application/json'})
 
-@app.route('/heroes', methods=['GET'])
-def heroes():
-    heroes = [x.to_dict(rules=('-hero_power',)) for x in Hero.query.all()]
-    return make_response(heroes, 200, {'Content-Type': 'application/json'})
+@api.route('/heroes', methods=['GET'])
+class Heroes(Resource):
+    def get(self):
+        heroes = [x.to_dict(rules=('-hero_power',)) for x in Hero.query.all()]
+        return make_response(heroes, 200, {'Content-Type': 'application/json'})
 
-@app.route('/heroes/<int:id>', methods=['GET'])
-def get_hero(id):
-    
-    hero = Hero.query.filter_by(id=id).first()
-    
-    if not hero:
-        res = {'error': 'Hero not found'}
-        code = 404
-    else:
-        res = hero.to_dict()
-        code = 200
+
+@api.route('/heroes/<int:id>', methods=['GET'])
+class HeroByID(Resource):
+    def get(self, id):
+        hero = Hero.query.filter_by(id=id).first()
         
-    return make_response(res, code, {'Content-Type': 'application/json'})
+        if not hero:
+            res = {'error': 'Hero not found'}
+            code = 404
+        else:
+            res = hero.to_dict(rules=('-hero_power',))
+            code = 200
+            
+        return make_response(res, code, {'Content-Type': 'application/json'})
 
-@app.route('/powers', methods=['GET'])
-def powers():
-    
-    powers = [x.to_dict(rules=('-hero_power',)) for x in Power.query.all()]
-    return make_response(powers, 200, {'Content-Type': 'application/json'})
 
-@app.route('/powers/<int:id>', methods=['GET', 'PATCH'])
-def get_power(id):
-    if request.method == 'GET':
+@api.route('/powers', methods=['GET'])
+class Powers(Resource):
+    def get(self):
+        powers = [x.to_dict(rules=('-hero_power',)) for x in Power.query.all()]
+        return make_response(powers, 200, {'Content-Type': 'application/json'})
+
+
+@api.route('/powers/<int:id>', methods=['GET', 'PATCH'])
+class PowerByID(Resource):
+    def get(self, id):
         power = Power.query.filter_by(id=id).first()
         
         if not power:
@@ -56,7 +63,7 @@ def get_power(id):
             
         return make_response(res, code, {'Content-Type': 'application/json'})
     
-    elif request.method == 'PATCH':
+    def patch(self, id):
         power = Power.query.filter_by(id=id).first()
         
         if not power:
@@ -77,23 +84,24 @@ def get_power(id):
                 
         return make_response(res, code, {'Content-Type': 'application/json'})
 
-@app.route('/hero_powers', methods=['POST'])
-def create_hero_power():
-    try:
-        json = request.get_json()
-        hero_power = HeroPower(strength=json['strength'], hero_id=json['hero_id'], power_id=json['power_id'])
-        
-        db.session.add(hero_power)
-        db.session.commit()
-        
-        res = hero_power.to_dict()
-        code = 201
-        
-    except Exception as e:
-        res = {'errors': ["validation errors"]}
-        code = 400
-        
-    return make_response(res, code, {'Content-Type': 'application/json'})
+@api.route('/hero_powers', methods=['POST'])
+class HeroPowers(Resource):
+    def post(self):
+        try:
+            json = request.get_json()
+            hero_power = HeroPower(strength=json['strength'], hero_id=json['hero_id'], power_id=json['power_id'])
+            
+            db.session.add(hero_power)
+            db.session.commit()
+            
+            res = hero_power.to_dict()
+            code = 201
+            
+        except Exception as e:
+            res = {'errors': ["validation errors"]}
+            code = 400
+            
+        return make_response(res, code, {'Content-Type': 'application/json'})
 
 if __name__ == "__main__":
     app.run(debug=True, port=5555)
